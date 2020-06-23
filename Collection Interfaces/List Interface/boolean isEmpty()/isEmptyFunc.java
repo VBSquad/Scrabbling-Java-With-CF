7f10723b33776f321ad54c2699f306ae0eb6064a
Akansha Jain <jakansha2001@gmail.com>
2020-06-23 16:26:14 +0530

    Modified isEmptyFunc.java

--- Collection Interfaces/List Interface/boolean isEmpty()/isEmptyFunc.java
+++ Collection Interfaces/List Interface/boolean isEmpty()/isEmptyFunc.java
@@ -1,22 +1,36 @@
 import java.util.*; 
 
 public class isEmptyFunc { 
-	public static void main(String[] args) 
+	public static void main ( String [ ] args ) 
 	{ 
-		List<Integer> arr = new ArrayList<Integer>(15); 
- 
-		boolean answer = arr.isEmpty(); 
-		if (answer == true) 
-			System.out.println("The List is empty"); 
+                // creating an Empty Integer List
+		List < Integer > arr = new ArrayList < Integer > ( 15 ) ; 
+                
+                // check if the list is empty or not 
+               // using isEmpty ( ) fucntion
+		boolean answer = arr.isEmpty ( ) ;  
+		if ( answer == true ) 
+			System.out.println ( "The List is empty" ) ; 
 		else
-			System.out.println("The List is not empty");
+			System.out.println ( "The List is not empty" ) ;
 
-		arr.add(16); 
- 
-		answer = arr.isEmpty(); 
+
+                // addition of a element to 
+               // the List
+		arr.add ( 16 ) ; 
+
+
+                // check if the list is empty or not 
+               // after adding an element 
+		answer = arr.isEmpty ( ) ; 
 		if (answer == true) 
-			System.out.println("The List is empty"); 
+			System.out.println ( "The List is empty" ) ; 
 		else
-			System.out.println("The List is not empty"); 
+			System.out.println ( "The List is not empty" ) ; 
 	} 
 } 
+
+
+/*  Output - 
+The List is empty
+The List is not empty */
