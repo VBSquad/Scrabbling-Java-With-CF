70cd32bd2d4c834d1418b5710e917139d9bc00cf
Vatsal kesarwani <vatsalkesarwani12@gmail.com>
2020-06-23 19:43:39 +0530

    Update Solution.java

--- Collection Interfaces/List Interface/Solution.java
+++ Collection Interfaces/List Interface/Solution.java
@@ -1,34 +1,54 @@
+/*
+E remove(int index): The method of List interface in Java is used to remove an element from the specified index from a List container and returns the element after removing it.
+It also shifts the elements after the removed element by 1 position to the left in the List.
+*/
+
 import java.util.*;
 
-public class boardMoves{
+public class Solution {
+	
     //main code
-    public static void main (String[] args)
+    public static void main ( String[ ] args )
     {
 	Scanner sc=new Scanner();
 	    
 	//n is the size of arrayList
-	int n=sc.nextInt();
+	int n = sc.nextInt();
 	    
         // Declare an empty List of size n
-        List<Integer> list = new ArrayList<Integer>(); 
+        List < Integer > list = new ArrayList < Integer > (); 
   
         // Add elements to the list 
-        for(int i=0;i<n;i++){
-            list.add(sc.nextInt());
+        for(int i = 0 ; i < n ; i++ ){
+            list.add( sc.nextInt() );
         }
   
         // Index from which you want to remove element 
-        int index = 2; 
+        int index = 2 ; 
   
         // Initial list 
-        System.out.println("Initial List: " + list); 
+        System.out.println( " Initial List : " + list ); 
   
         // remove element 
-        list.remove(index); 
+        list.remove( index ) ; 
   
         // Final list 
-        System.out.println("Final List: " + list); 
+        System.out.println( " Final List : " + list ); 
         
 	sc.close();
     }
 }
+
+/*
+
+Sample Test Case
+
+Input :
+5
+1 2 3 4 5
+
+Output :
+Initial List : [ 1 , 2 , 3 , 4 , 5 ]
+Final List : [ 1 , 2 , 4 , 5 ]
+
+*/
