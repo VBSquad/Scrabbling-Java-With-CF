5c432cdbdd7377d2da5d3eac89e6d783a18fe98f
iamvatsal <vatsalkesarwani12@gmail.com>
2020-07-21 03:18:37 +0530

    correction

--- Collection Classes/Vector Class/int indexOf(Object elem, int index)/int indexOf(Object elem, int index).java
+++ Collection Classes/Vector Class/int indexOf(Object elem, int index)/int indexOf(Object elem, int index).java
@@ -1,5 +1,5 @@
 /*
- int indexOf(Object o): This method returns the index of the first occurrence of the specified element in this vector, or -1 if this vector does not contain the element.
+ int indexOf(Object elem, int index): This method returns the index of the first occurrence of the specified element in this vector, or -1 if this vector does not contain the element after the given index position.
 
 */
 
@@ -29,8 +29,8 @@ class VectorIndexOf {
         
         System.out.println( " Vector : " + vc );
         
-        // get the element at index of Geeks 
-        System.out.println(" index of 'in' is: " + vc.indexOf("in") );
+        // get the element at index of in after 3 postion 
+        System.out.println(" index of 'in' after index 3 is: " + vc.indexOf("in" , 3) );
         
 	sc.close();
 	
@@ -42,10 +42,10 @@ class VectorIndexOf {
 Sample Test Case
 
 Input :
-5
-student code in open source
+6
+student code in open source in
 
 Output:
-Vector : [student, code, in, open, source]
-index of 'in' is: 2
+ Vector : [student, code, in, open, source, in]
+ index of 'in' after index 3 is: 5
 */
