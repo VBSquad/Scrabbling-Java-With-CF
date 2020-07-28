2b048de1de2549c9790a6d25bce411178311f648
Dhruv Mehta <61137052+Dhruv-194@users.noreply.github.com>
2020-07-29 02:13:11 +0530

    Update boolean contains(Object o).java
    added user input for checking also

--- Collection Classes/Linkedlist Class/boolean contains(Object o)/boolean contains(Object o).java
+++ Collection Classes/Linkedlist Class/boolean contains(Object o)/boolean contains(Object o).java
@@ -1,45 +1,52 @@
 /**
   boolean contains() : method is used to check whether an element is present in a LinkedList or not. It takes the element as a parameter and returns True if the element is present in the list.
  */
- 
+
 import java.io.*; 
 import java.util.*;
-  
+
 class LinkedListContains { 
-    
+
    public static void main(String args[]) { 
-       
+
        Scanner sc = new Scanner( System.in );
-       
+
        //size of Linked List
        int n = sc.nextInt();
-       
+
       // creating an empty LinkedList 
       LinkedList list = new LinkedList(); 
-      
-  
+
+
        //input n elemets in linked list 
        for(int i = 0 ; i < n ; i++ ){
-           
+
            list.add( sc.next() );
-           
+
        }
-  
+
       // Output the present list 
       System.out.println( "The list is:" + list ); 
-  
+      
+      //Taking input for what user wants to check
+      System.out.println("Enter what you want to check -");
+      
+      String p = sc.next();
+
       // Check if the list contains "Student" 
-      System.out.println( "Does the List contains 'Student': "                          + list.contains("Student"));
-  
+      System.out.println( "Does the List contains '" + p + "':"                          + list.contains(p));
+
    } 
 } 
 
+
 /**
 Input:
-3
-Student Code In
-
+4
+I lost my keys
+Enter what you want to check -
+lost
 Output:
-The list is:[Student, Code, In]
-Does the List contains 'Student': true
- */
+The list is:[I, lost, my, keys]
+Does the List contains 'lost':true
+*/ 
