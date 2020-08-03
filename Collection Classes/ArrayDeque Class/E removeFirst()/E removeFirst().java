b5c5254de6169bcb393ac5f6d7aac756575a43ae
iamvatsal <vatsalkesarwani12@gmail.com>
2020-08-04 00:22:25 +0530

    Implement ArrayDeque Class - E removeFirst()

---
+++ Collection Classes/ArrayDeque Class/E removeFirst()/E removeFirst().java
@@ -0,0 +1,46 @@
+/**
+  E removeFirst() : method returns first element and also removes it
+*/
+ 
+import java.io.*; 
+import java.util.*;
+  
+class ArrayDequeRemoveFirst { 
+    
+   public static void main(String args[]) { 
+       
+       Scanner sc = new Scanner( System.in );
+       
+       //size of deque
+       int n = sc.nextInt();
+       
+       // Intializing an deque 
+        Deque<String> d = new ArrayDeque<>();
+      
+  
+       //input n elemets in Deque 
+       for(int i = 0 ; i < n ; i++ ){
+           
+           d.add( sc.next() );
+           
+       }
+  
+      // Output of the present Deque
+      System.out.println( "The Deque is : " + d ); 
+      
+      d.removeFirst();
+      
+      //Size of Deque
+      System.out.println( "Final Deque : " + d );
+   } 
+} 
+
+/**
+Input:
+3
+Student Code In
+
+Output:
+The Deque is : [Student, Code, In]
+Final Deque : [Code, In]
+ */
