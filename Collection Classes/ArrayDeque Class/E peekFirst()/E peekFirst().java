c945b21acd6487058a67119ce25ed01803d30fc1
iamvatsal <vatsalkesarwani12@gmail.com>
2020-08-05 12:36:03 +0530

    Implement ArrayDeque Class - E peekFirst()

---
+++ Collection Classes/ArrayDeque Class/E peekFirst()/E peekFirst().java
@@ -0,0 +1,46 @@
+/**
+  E peekFirst() : method returns first element without removing it.
+*/
+ 
+import java.io.*; 
+import java.util.*;
+  
+class ArrayDequePeekFirst { 
+    
+   public static void main(String args[]) { 
+       
+       Scanner sc = new Scanner( System.in );
+       
+       //size of deque
+       int n = sc.nextInt();
+       
+       // Intializing an deque 
+        Deque<String> d = new ArrayDeque<>();
+      
+  
+       //input n elemets in Deque 
+       for(int i = 0 ; i < n ; i++ ){
+           
+           d.push( sc.next() );
+           
+       }
+  
+      // Output of the present Deque
+      System.out.println( "The Deque is : " + d ); 
+      
+      //pollLast element
+      System.out.println( "Element At First : " + d.peekFirst() );
+     
+      
+   } 
+} 
+
+/**
+Input:
+3
+Student Code In
+
+Output:
+The Deque is : [In, Code, Student]
+Element At First : In
+ */
