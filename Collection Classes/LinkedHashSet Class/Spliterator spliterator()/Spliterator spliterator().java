145449c9bbd65ab43f44bc2b243f212b9593e5f8
Jyoti Jauhari <jyotijauhari222@gmail.com>
2020-08-17 21:44:45 +0530

    Update Spliterator spliterator().java

--- Collection Classes/LinkedHashSet Class/Spliterator spliterator()/Spliterator spliterator().java
+++ Collection Classes/LinkedHashSet Class/Spliterator spliterator()/Spliterator spliterator().java
@@ -2,39 +2,41 @@
  Spliterator spliterator() :  method of Java LinkedHashSet class makes a late-binding and fail-fast Spliterator across the elements of this set.
 
  */
-import java.util.*;
 
-class LinkedHashSetSpliterator {
-    static int i=1;  
+import java.util.*;
+public class Main
+{
+    static int i=1; 
     public static void main(String args[]) {
-      
+     
       Scanner sc = new Scanner( System.in );
-      
+
       //size of LinkedHashSet
       int nn = sc.nextInt();
-      
+
         LinkedHashSet hs = new LinkedHashSet();  
-  
+
         // Mapping string values to int keys
-        for( int i = 0 ; i < nn ; i++ ){
-            
+        for(i = 0 ; i < nn ; i++ ){
+
             hs.add( sc.next() );
-        
+
         }
-  
+
         // Displaying the LinkedHashSet 
         System.out.println( "Initial Mappings are: " + hs); 
-        
+
         Spliterator<String> str = hs.spliterator();  
-  
+
         // if element exists tryAdvance() will perform action  
-        
+        i = 0;
         while(
             str.tryAdvance( (n)->
-            System.out.println( i++ + " Alphabet - " + n ) ) );
-        
-   }    
+            System.out.println( ++i + " Alphabet - " + n ) ) );
+
+   }  
 }
+
 /**
 Input:
 3
